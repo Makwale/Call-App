c442f3a27bf8b25c29dcc6e7139b3cb3e734af46
Makwale <makwale.em@gmail.com>
2021-01-26 07:24:37 -0800

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { CallNumber } from '@ionic-native/call-number/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,11 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private callNumber: CallNumber) {}
+
+  call(){
+    this.callNumber.callNumber("0798374033", true).then(res => alert('Launched dialer!' + res))
+  .catch(err => alert('Error launching dialer' + err.message));
+  }
 
 }
